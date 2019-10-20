f6ef56abb92c002cd6bce44974acd4f27227ef18
tomoya_chen <tomoya_chen@163.com>
2019-10-20 16:54:12 +0800

    DIY

--- manage.py
+++ manage.py
@@ -1,8 +1,14 @@
 #-*- coding:utf-8 -*-
 # author:Agam
 # datetime:2018-11-05
-
+import os
+from dotenv import load_dotenv
 from app.apps import app
 
+#不使用Flask内置服务器时，需要手动加载环境变量
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',debug=True,port=5050)
+    app.run(host='127.0.0.1',debug=True,port=5001)
